89a1fb60138b5ffdaa48e8065586c0dbafd01596
Martin Mädler <martin.maedler@gmail.com>
2017-08-14 16:27:44 +0200

    Added some text to force a scroll bar.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  text = `This is a text paragraph.
+    It is just here to fill up some space.
+    Bla bla bla Yada yada yada.
+    And repeat.`;
 }
